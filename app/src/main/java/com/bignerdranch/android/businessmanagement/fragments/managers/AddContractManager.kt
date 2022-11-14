d40a6e6bbec5685dc599aaff07cad4809dcc2991
Yixian Wang <wangyixian.wyx@gmail.com>
2022-11-14 01:33:19 -0500

    add date picker

--- app/src/main/java/com/bignerdranch/android/businessmanagement/fragments/managers/AddContractManager.kt
+++ app/src/main/java/com/bignerdranch/android/businessmanagement/fragments/managers/AddContractManager.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.businessmanagement.fragments.managers
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.businessmanagement.MainViewModel
 import androidx.activity.viewModels
@@ -21,13 +22,16 @@ class AddContractManager : AppCompatActivity() {
                 binding.etTitle.text.toString(),
                 binding.etLocation.text.toString(),
                 binding.etRent.text.toString(),
-                binding.etStart.text.toString(),
-                binding.etEnd.text.toString(),
+                "${binding.etStartDate.month + 1}/${binding.etStartDate.dayOfMonth}/${binding.etStartDate.year}",
+                "${binding.etEndDate.month + 1}/${binding.etEndDate.dayOfMonth}/${binding.etEndDate.year}",
                 binding.etDuration.text.toString(),
                 binding.etNote.text.toString(),
                 binding.etName.text.toString(),
                 binding.etPhone.text.toString()
             )
+
+//            Toast.makeText(applicationContext, "${binding.etStartDate.month + 1}/${binding.etStartDate.dayOfMonth}/${binding.etStartDate.year}", Toast.LENGTH_SHORT).show()
+
 
             Intent().apply {
                 setResult(RESULT_OK, this)
