d3fdbe7a422d1d91d2b5da1740d736625700fa60
Yixian Wang <wangyixian.wyx@gmail.com>
2022-11-15 14:15:52 -0500

    add table to accountant programmatically

--- app/src/main/java/com/bignerdranch/android/businessmanagement/fragments/AccountantFragment.kt
+++ app/src/main/java/com/bignerdranch/android/businessmanagement/fragments/AccountantFragment.kt
@@ -1,13 +1,18 @@
 package com.bignerdranch.android.businessmanagement.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.bignerdranch.android.businessmanagement.R
 import com.bignerdranch.android.businessmanagement.databinding.FragmentAccountantBinding
-import com.bignerdranch.android.businessmanagement.databinding.FragmentHomeBinding
 
 
 class AccountantFragment : Fragment(R.layout.fragment_accountant) {
@@ -25,5 +30,85 @@ class AccountantFragment : Fragment(R.layout.fragment_accountant) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAccountantBinding.bind(view)
 
+        val ll = binding.accountantTable
+
+        for (i in 0..1) {
+            val row = TableRow(context)
+            row.layoutParams = TableRow.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT,
+            )
+
+            val tv1 = TextView(context)
+            val tv2 = TextView(context)
+            val tv3 = TextView(context)
+
+            tv1.text = "a" + i
+            tv2.text = "b" + i
+            tv3.text = "c" + i
+
+
+            tv1.layoutParams = TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT,
+                4f
+            )
+
+            tv2.layoutParams = TableRow.LayoutParams(
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    2f
+            )
+
+            tv3.layoutParams = TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT,
+                4f
+            )
+
+            tv1.gravity = Gravity.CENTER_HORIZONTAL
+            tv2.gravity = Gravity.CENTER_HORIZONTAL
+            tv3.gravity = Gravity.CENTER_HORIZONTAL
+
+//            row.addView(tv1, -1, TableLayout.LayoutParams(
+//                TableRow.LayoutParams.WRAP_CONTENT,
+//                TableRow.LayoutParams.WRAP_CONTENT,
+//                4f
+//            ))
+//            row.addView(tv2, -1, TableLayout.LayoutParams(
+//                TableRow.LayoutParams.WRAP_CONTENT,
+//                TableRow.LayoutParams.WRAP_CONTENT,
+//                2f
+//            ))
+//            row.addView(tv3, -1, TableLayout.LayoutParams(
+//                TableRow.LayoutParams.WRAP_CONTENT,
+//                TableRow.LayoutParams.WRAP_CONTENT,
+//                4f
+//            ))
+
+            row.addView(tv1)
+            row.addView(tv2)
+            row.addView(tv3)
+
+
+
+
+
+//            checkBox = CheckBox(this)
+//            tv = TextView(this)
+//            addBtn = ImageButton(this)
+//            addBtn.setImageResource(R.drawable.add)
+//            minusBtn = ImageButton(this)
+//            minusBtn.setImageResource(R.drawable.minus)
+//            qty = TextView(this)
+//            checkBox.setText("hello")
+//            qty.setText("10")
+//            row.addView(checkBox)
+//            row.addView(minusBtn)
+//            row.addView(qty)
+//            row.addView(addBtn)
+
+            ll.addView(row, -1)
+        }
     }
 }
