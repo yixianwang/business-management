261be6aa481cd633dac64fd909496157134ca135
Yixian Wang <wangyixian.wyx@gmail.com>
2022-11-15 12:25:09 -0500

    add protype of linechart

--- app/src/main/java/com/bignerdranch/android/businessmanagement/fragments/DataFragment.kt
+++ app/src/main/java/com/bignerdranch/android/businessmanagement/fragments/DataFragment.kt
@@ -1,13 +1,18 @@
 package com.bignerdranch.android.businessmanagement.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.bignerdranch.android.businessmanagement.R
 import com.bignerdranch.android.businessmanagement.databinding.FragmentCurrentBinding
 import com.bignerdranch.android.businessmanagement.databinding.FragmentDataBinding
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 class DataFragment : Fragment(R.layout.fragment_data) {
     companion object {
@@ -25,6 +30,33 @@ class DataFragment : Fragment(R.layout.fragment_data) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentDataBinding.bind(view)
 
+        setLineChartData()
+    }
+
+    fun setLineChartData() {
+        val xvalue = ArrayList<String>()
+        xvalue.add("11.00 am")
+        xvalue.add("12.00 am")
+        xvalue.add("1.00 am")
+        xvalue.add("3.00 am")
+        xvalue.add("5.00 am")
+
+        val lineEntry = ArrayList<Entry>();
+        lineEntry.add(Entry(20f, 10f))
+        lineEntry.add(Entry(30f, 1f))
+        lineEntry.add(Entry(40f, 2f))
+        lineEntry.add(Entry(50f, 3f))
+        lineEntry.add(Entry(60f, 4f))
+
+        val lineDataSet = LineDataSet(lineEntry, "First")
+        lineDataSet.color = ContextCompat.getColor(requireContext(), R.color.colorPrimary)
+
+//        val data = LineData(xvalue, lineDataSet)
+        val data = LineData(lineDataSet)
+
+        binding.lineChart.data = data
+        binding.lineChart.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+//        binding.lineChart.animateXY(3000, 3000)
     }
 }
 
